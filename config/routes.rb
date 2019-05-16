e282985c1b6975f2c0c3623f59fb69f71dbc01cb
Amani Anai <amani@dfi.agency>
2019-05-16 01:16:01 -0500

    layout setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/faq'
+
+  get 'pages/terms'
+
+  get 'pages/privacy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
