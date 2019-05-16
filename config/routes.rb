83d020a0fe9ebf0d3df38a4da7616713c0e76094
Amani Anai <amani@dfi.agency>
2019-05-16 17:22:30 -0500

    steps added

--- config/routes.rb
+++ config/routes.rb
@@ -1,54 +1,51 @@
 Rails.application.routes.draw do
 
-  get 'step_6/home'
-  get 'step_6/tool_1'
-  get 'step_6/tool_2'
-  get 'step_6/tool_3'
-  get 'step_6/tool_4'
-
-  get 'step_5/home'
-  get 'step_5/inquiry_1'
-  get 'step_5/inquiry_2'
-  get 'step_5/inquiry_3'
-  get 'step_5/inquiry_4'
-
-  get 'step_4/home'
-  get 'step_4/concepts_1'
-  get 'step_4/concepts_2'
-  get 'step_4/concepts_3'
-  get 'step_4/concepts_4'
-
-  get 'step_3/home'
-  get 'step_3/scope_1'
-  get 'step_3/scope_2'
-  get 'step_3/scope_3'
-  get 'step_3/scope_4'
-
-  get 'step_2/home'
-  get 'step_2/context_1'
-  get 'step_2/context_2'
-  get 'step_2/context_3'
-  get 'step_2/context_4'
-
-  get 'step_1/home'
-  get 'step_1/grounding_1'
-  get 'step_1/grounding_2'
-  get 'step_1/grounding_3'
-  get 'step_1/grounding_4'
-
   root 'pages#index'
 
-  get 'pages/index'
+  get 'pages/index'       => 'pages#index',       as: :home
+  get 'pages/about'       => 'pages#about',       as: :about
+  get 'pages/contact'     => 'pages#contact',     as: :contact
+  get 'pages/faq'         => 'pages#faq',         as: :faq
+  get 'pages/terms'       => 'pages#terms',       as: :terms
+  get 'pages/privacy'     => 'pages#privacy',     as: :privacy
 
-  get 'pages/about'
+  get 'step6/home'       => 'step6#home',       as: :tool_home
+  get 'step6/tool_1'     => 'step6#tool_1',     as: :tool_1
+  get 'step6/tool_2'     => 'step6#tool_2',     as: :tool_2
+  get 'step6/tool_3'     => 'step6#tool_3',     as: :tool_3
+  get 'step6/tool_4'     => 'step6#tool_4',     as: :tool_4
 
-  get 'pages/contact'
+  get 'step5/home'       => 'step_5#home',       as: :inquiry_home
+  get 'step5/inquiry_1'  => 'step5#inquiry_1',  as: :inquiry_1
+  get 'step5/inquiry_2'  => 'step5#inquiry_2',  as: :inquiry_2
+  get 'step5/inquiry_3'  => 'step5#inquiry_3',  as: :inquiry_3
+  get 'step5/inquiry_4'  => 'step5#inquiry_4',  as: :inquiry_4
 
-  get 'pages/faq'
+  get 'step4/home'
+  get 'step4/concepts_1'
+  get 'step4/concepts_2'
+  get 'step4/concepts_3'
+  get 'step4/concepts_4'
 
-  get 'pages/terms'
+  get 'step3/home'
+  get 'step3/scope_1'
+  get 'step3/scope_2'
+  get 'step3/scope_3'
+  get 'step3/scope_4'
 
-  get 'pages/privacy'
+  get 'step2/home'
+  get 'step2/context_1'
+  get 'step2/context_2'
+  get 'step2/context_3'
+  get 'step2/context_4'
+
+  get 'step1/home'          => 'step_1#home',         as: :grounding_home
+  get 'step1/grounding_1'   => 'step_1#grounding_1',  as: :grounding_1
+  get 'step1/grounding_2'   => 'step_1#grounding_2',  as: :grounding_2
+  get 'step1/grounding_3'   => 'step_1#grounding_3',  as: :grounding_3
+  get 'step1/grounding_4'   => 'step_1#grounding_4',  as: :grounding_4
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
