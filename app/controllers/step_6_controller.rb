c95b677345cd3d59731ba59725040f5a635755c3
Amani Anai <amani@dfi.agency>
2019-05-16 16:48:43 -0500

    routes updated

---
+++ app/controllers/step_6_controller.rb
@@ -0,0 +1,16 @@
+class Step6Controller < ApplicationController
+  def home
+  end
+
+  def tool_1
+  end
+
+  def tool_2
+  end
+
+  def tool_3
+  end
+
+  def tool_4
+  end
+end
---
+++ app/helpers/step_6_helper.rb
@@ -0,0 +1,2 @@
+module Step6Helper
+end
